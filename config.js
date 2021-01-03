2d7632774f4b1c168199d2006c6638e6de202075
beron <bleink@outlook.es>
2021-01-03 23:41:57 +0100

    config.js - add allowedUrls & [allowedHeaders, allowedMethods, secret] whose added to config constants by getCompleteConfig func

--- config.js
+++ config.js
@@ -4,18 +4,21 @@ const mode = process.env.ENV_MODE
 
 const development = {
     mode,
+    allowedUrls:process.env.DEV_ALLOWED_URLS.split(','),
     codeshipApi: {
         hostname: 'localhost'
     }
 }
 const test = {
     mode,
+    allowedUrls:process.env.DEV_ALLOWED_URLS.split(','),
     codeshipApi: {
         hostname: 'localhost'
     }
 }
 const production = {
     mode,
+    allowedUrls:process.env.ALLOWED_URLS.split(','),
     codeshipApi: {
         hostname: 'codeship-api.herokuapp.com'
     }
@@ -24,9 +27,22 @@ const production = {
 export function getConfig() {
     switch(mode) {
     case "DEV" :
-    return development;
+    return getCompleteConfig(development);
     case "TEST" :
-    return test;
+    return getCompleteConfig(test);
     default :
-    return production;
+    return getCompleteConfig(production);
 }}
+
+
+/**
+ * returns config obj with default properties
+ * @param {object} config 
+ */
+function getCompleteConfig(config) {
+    return Object.assign(config, {
+        allowedHeaders:process.env.ALLOWED_HEADERS,
+        allowedMethods:process.env.ALLOWED_METHODS,
+        secret: process.env.SECRET
+    });
+}
