bf211830954e2d2b319e5abffeba25eeb036fe2e
beron <bleink@outlook.es>
2021-01-03 23:47:03 +0100

    config.js - add port

--- config.js
+++ config.js
@@ -3,21 +3,18 @@ import 'dotenv/config.js'
 const mode = process.env.ENV_MODE
 
 const development = {
-    mode,
     allowedUrls:process.env.DEV_ALLOWED_URLS.split(','),
     codeshipApi: {
         hostname: 'localhost'
     }
 }
 const test = {
-    mode,
     allowedUrls:process.env.DEV_ALLOWED_URLS.split(','),
     codeshipApi: {
         hostname: 'localhost'
     }
 }
 const production = {
-    mode,
     allowedUrls:process.env.ALLOWED_URLS.split(','),
     codeshipApi: {
         hostname: 'codeship-api.herokuapp.com'
@@ -41,6 +38,8 @@ export function getConfig() {
  */
 function getCompleteConfig(config) {
     return Object.assign(config, {
+        mode,
+        port: process.env.PORT,
         allowedHeaders:process.env.ALLOWED_HEADERS,
         allowedMethods:process.env.ALLOWED_METHODS,
         secret: process.env.SECRET
