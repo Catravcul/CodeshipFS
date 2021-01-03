87160c2c9ebd829a932570950a61e472dafa87c6
beron <bleink@outlook.es>
2021-01-03 18:34:47 +0100

    server.js - add middleware to get img_path by token with https request to api

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import 'dotenv/config.js'
 import { join } from 'path'
+import { get, request } from 'https'
 import fileupload from 'express-fileupload'
 import express from 'express'
 const route = express()
@@ -10,6 +11,27 @@ route.use(fileupload({
     useTempFiles: true,
     tempFileDir: '/tmp/'
 }))
+
+route.use((req, res, next) => {
+    const token = req.header('x-access-token')
+    if (!token) res.status(300).json({msg: 'A token is required'})
+    const options = {
+        hostname: 'codeship-api.herokuapp.com',
+        port: 443,
+        path: '/token/img_path',
+        method: 'GET',
+        headers: {'x-access-token': token}
+    }
+    get(options, request => {
+        let response = ''
+        request.on('data', data => response += data)
+        request.on('end', () => {
+            req.img_path = JSON.parse(response).img_path
+            next()
+        })
+        request.on('error', err => console.log(err))
+    }).end()
+})
 
 route.put('/', (req, res) => {
     req.files.img.mv('public' + req.body.path)
