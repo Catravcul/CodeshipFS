fe6741fcf1afcd2ffcb3368e013bb02cf09186ca
beron <bleink@outlook.es>
2021-01-04 16:48:45 +0100

    userController.js - add unlink to update to erase old photo

--- user/userController.js
+++ user/userController.js
@@ -1,33 +1,46 @@
+import { unlink } from 'fs'
 import User from './userModel.js'
+
 export const update = async (req, res) => {
     try {
         if (req.files.img) {
-            const body = {img_path: '/img/user/' + req.username + getExt(req.files.img.mimetype)}
-            const updated = await User.findByIdAndUpdate(
-              req.id,
-              body,
-              {
-                new: true,
-                runValidators: true,
-              }
-            )
-            let mvErr
-            req.files.img.mv('public' + body.img_path, err => mvErr = err)
-            res.status(200).json({
-              user: updated,
-              err: mvErr
-            })
+            const user = await User.findById(req.id)
+            unlink('./public' + user.img_path, update)
         } else {
             res.status(400).json({
               err: 'img file could not be found in files'
             })
         }
-      } catch (err) {
+    } catch (err) {
         res.status(400).json({
-          err: err.message
+            err: err.message
         })
-      }
-  }
+    }
+
+    async function update(err) {
+        if (err) {
+            console.log(err)
+            res.status(500).json({err})
+        } else {
+            const body = {img_path: '/img/user/' + req.username + getExt(req.files.img.mimetype)}
+            const updated = await User.findByIdAndUpdate(
+            req.id,
+            body,
+            {
+                new: true,
+                runValidators: true,
+            }
+            )
+            let mvErr
+            req.files.img.mv('public' + body.img_path, err => mvErr = err)
+            res.status(200).json({
+            user: updated,
+            err: mvErr
+            })
+        }
+    }
+}
+      
 /**
  * return extention pending on mimetype
  * @param {string} req 
