d5aeb56fd1381eb7e037fb3f22265f3e9b123116
beron <bleink@outlook.es>
2021-01-04 14:32:26 +0100

    config.js - db added

--- config.js
+++ config.js
@@ -3,18 +3,21 @@ import 'dotenv/config.js'
 const mode = process.env.ENV_MODE
 
 const development = {
+    DB: process.env.DATABASE_LOCAL,
     allowedUrls:process.env.DEV_ALLOWED_URLS.split(','),
     codeshipApi: {
         hostname: 'localhost'
     }
 }
 const test = {
+    DB: process.env.DATABASE_LOCAL,
     allowedUrls:process.env.DEV_ALLOWED_URLS.split(','),
     codeshipApi: {
         hostname: 'localhost'
     }
 }
 const production = {
+    DB: process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD),
     allowedUrls:process.env.ALLOWED_URLS.split(','),
     codeshipApi: {
         hostname: 'codeship-api.herokuapp.com'
